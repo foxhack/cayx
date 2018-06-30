f35393f49493d376787f5118134c63a02089c520
Zhou Yin <zhou.yin@ihaola.com.cn>
2018-06-30 18:04:00 +0800

    refactor: 授权页方法重构，防止某些方法再微信重定向后又调用一次

--- src/views/user/author.vue
+++ src/views/user/author.vue
@@ -2,13 +2,13 @@
   <div></div>
 </template>
 <script>
-  import { guideToAuth, getQueryString } from '@/utils/common'
+  import { guideToWxAuth, getQueryString } from '@/utils/common'
   import { ENV } from '@/utils/env'
-
+  import { MockUserID } from '@/utils/config'
   export default{
     name    : 'Author',
     methods : {
-      getWxAuthor(){
+      getAuthor(){
         let code = getQueryString("code");
         let _ = this
         if (code) {
@@ -23,36 +23,33 @@
             _.$router.replace(window.localStorage.getItem('toPath'))
           }
         } else {
-          guideToAuth(_.$router.mode)
+          switch (ENV) {
+            case 'production':
+              guideToWxAuth(_.$router.mode)
+              break
+            case 'development':
+              this.$store.commit('setUserID', MockUserID)
+              this.$router.replace(window.localStorage.getItem('toPath'))
+              break
+            case 'test':
+              if (window.confirm('您当访问的是测试环境，要使用微信验证进行测试么?确定代表使用，取消代表不使用。')) {
+                guideToWxAuth(_.$router.mode)
+              } else {
+                this.$store.commit('setUserID', MockUserID)
+                this.$router.replace(window.localStorage.getItem('toPath'))
+              }
+              break
+            default:
+              console.log('出错了，没有匹配的环境。')
+          }
         }
-      },
-      isWeiXin(){
-        let ua = window.navigator.userAgent.toLowerCase();
-        return ua.match(/MicroMessenger/i)=='micromessenger'
       }
     },
     created(){
       console.log('进入授权页')
       console.log(window.localStorage.getItem('toPath'))
-      switch (ENV){
-        case 'production':
-          this.getWxAuthor()
-          break
-        case 'development':
-          this.$store.commit('setUserID','ef510d48a33f43c5af49dca988cb7ba2')
-          this.$router.replace(window.localStorage.getItem('toPath'))
-          break
-        case 'test':
-          if(window.confirm('您当访问的是测试环境，要使用微信验证进行测试么?确定代表使用，取消代表不使用。')){
-            this.getWxAuthor()
-          }else{
-            this.$store.commit('setUserID','ef510d48a33f43c5af49dca988cb7ba2')
-            this.$router.replace(window.localStorage.getItem('toPath'))
-          }
-          break
-        default:
-          console.log('出错了，没有匹配的环境。')
-      }
+      this.getAuthor()
+
     }
   }
 </script>
