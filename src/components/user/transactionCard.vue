edb7ee82bfb1bec4c970f09e3d475d74bd9a89ba
Zhou Yin <zhou.yin@ihaola.com.cn>
2018-06-30 18:11:41 +0800

    new feature: 根据需求，调整了我的理财中关于我的资产栏目的页面

---
+++ src/components/user/transactionCard.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="trans-card-wrapper">
+    <div class="header flex-row bottom-line">
+      <div class="left">{{transaction.productName}}</div>
+      <div class="right">{{transaction.averageRate}}%</div>
+    </div>
+    <div class="main flex-col">
+      <div class="line-1 flex-row">
+        <span>{{transaction.name}}</span>
+        <span>{{transaction.amount|money}}元</span>
+      </div>
+      <div class="line-2 flex-row">
+        <span v-if="transaction.beginType">{{transaction.beginType}}：{{transaction.beginDate}}</span>
+        <span v-if="transaction.endType">{{transaction.endType}}：{{transaction.endDate}}</span>
+      </div>
+      <div class="line-3 flex-row" v-if="transaction.error">
+        <span class="el-icon-error"></span>
+        <span class="error-tip">{{transaction.error.type}}：{{transaction.error.msg}}</span>
+      </div>
+    </div>
+    <div class="footer flex-row top-line">
+      <div class="left">交易流水&nbsp;12222223332400</div>
+      <slot name="operate"></slot>
+    </div>
+  </div>
+</template>
+<script>
+  export default{
+    name  : 'TransactionCard',
+    props : ['transaction']
+  }
+</script>
+<style lang="stylus" scoped>
+  @import '../../style/base.styl'
+
+  .trans-card-wrapper
+    color primary-dark-color
+    background white
+    font-size small
+    padding 10px 10px 0 10px
+    margin-bottom 5px
+    .header, .footer
+      position relative
+      min-height 3em
+      align-items center
+      color secondary-dark-color
+    .header, .footer, .main > .line-1, .main > .line-2
+      justify-content space-between
+    .main
+      min-height 5em
+      justify-content space-around
+      .line-2
+        font-size extra-extra-small
+        color neutral-text-color2
+    .footer .left
+      font-size extra-small
+      color neutral-text-color4
+  .el-icon-error
+    font-size extra-extra-small
+    color error-color
+    line-height inherit
+</style>
--- src/views/user/transactionRecord.vue
+++ src/views/user/transactionRecord.vue
@@ -1,75 +1,104 @@
 <template>
   <div class="page-with-top">
-    <mt-loadmore :top-method="loadTop"
-                 @top-status-change="handleTopChange"
-                 ref="loadmore">
-      <div slot="top" class="mint-loadmore-top">
-        <span v-show="topStatus !== 'loading'" :class="{ 'rotate': topStatus === 'drop' }">↓</span>
-        <span v-show="topStatus === 'loading'">
-          <mt-spinner type="fading-circle" color="#C49A16"></mt-spinner>
-        </span>
+    <div class="assets-summary flex-row">
+      <div>
+        <div>持有资产（元）</div>
+        <div></div>
+        100000.00
       </div>
+      <div>
+        <div>待收利息（元）</div>
+        <div></div>
+        200.00
+      </div>
+      <div>
+        <div>累计收益（元）</div>
+        <div>500.00</div>
+      </div>
+    </div>
+    <div class="tab-section flex-row">
+      <div :class="{active:currentTab==1}" @click="currentTab=1">持有中</div>
+      <div :class="{active:currentTab==2}" @click="changeTab(2)">购买中</div>
+      <div :class="{active:currentTab==3}" @click="changeTab(3)">已赎回</div>
+    </div>
+    <mt-loadmore>
       <div v-infinite-scroll="generateMock"
            infinite-scroll-disabled="loadMore"
            infinite-scroll-distance="10">
-        <mt-cell v-for="r,index in random"
-                 :title="r.date"
-                 :key="index" :label="r.type">
-          {{r.amount|money}}|{{index}}
-        </mt-cell>
+        <template v-if="this.currentTab==1">
+          <template v-for="r in random[0]">
+            <transaction-card :transaction="r">
+              <template slot="operate">
+                <div class="operate-btn">预约赎回</div>
+              </template>
+            </transaction-card>
+          </template>
+        </template>
+        <template v-if="this.currentTab==2">
+          <template v-for="r in random[1]">
+            <transaction-card :transaction="r">
+              <template slot="operate">
+                <div class="operate-btn">取消购买</div>
+              </template>
+            </transaction-card>
+          </template>
+        </template>
+        <template v-if="this.currentTab==3">
+          <template v-for="r in random[2]">
+            <transaction-card :transaction="r">
+              <template slot="operate"></template>
+            </transaction-card>
+          </template>
+        </template>
         <mt-spinner type="fading-circle" color="#C49A16" v-show="loadMore"></mt-spinner>
       </div>
     </mt-loadmore>
   </div>
 </template>
 <script>
-  import { Spinner, Loadmore } from 'mint-ui';
+  import { Spinner, Loadmore } from 'mint-ui'
+  import TransactionCard from '@/components/user/transactionCard'
   export default{
     data(){
       return {
-        loadMore     : false,
-        topStatus    : '',
-        pid          : this.$route.params.pid,
-        random       : []
+        loadMore   : false,
+        topStatus  : '',
+        random     : [[], [], []],
+        currentTab : 1,
       }
     },
     components : {
       'mt-spinner'  : Spinner,
-      'mt-loadmore' : Loadmore
+      'mt-loadmore' : Loadmore,
+      TransactionCard
     },
     methods    : {
-      loadTop(){
-        let random = []
-        for (let n = 0; n < 3; n++) {
-          random.push({
-            date   : '2018-2-'+(n+1),
-            type   : (Math.random(0, 1) > 0.5 ? '买入' : '卖出'),
-            amount : (Math.random(0, 1)*10000)
-          })
-        }
-        setTimeout(() => {
-          this.random = random.concat(this.random)
-          this.$refs.loadmore.onTopLoaded()
-        }, 2500)
-      },
-      handleTopChange(status) {
-        this.topStatus = status;
-        console.log(status)
+      changeTab(tab){
+        this.currentTab = tab
+        if (this.random[tab-1].length==0) this.generateMock()
       },
       generateMock(){
-        this.loadMore=true
+        this.loadMore = true
         let random = []
+        let name = this.currentTab==3 ? '赎回金额' : '购买金额'
+
         for (let n = 0; n < 20; n++) {
           random.push({
-            date   : '2018-1-'+(n+1),
-            type   : (Math.random(0, 1) > 0.5 ? '买入' : '卖出'),
-            amount : (Math.random(0, 1)*10000)
+            productName : '季季盈1号第'+n+'期',
+            name        : name,
+            averageRate : 4.95,
+            beginType   : '起息日',
+            beginDate   : '2018年1月'+(n+1)+'日',
+            endType     : '到期日',
+            endDate     : '2018年4月'+(n+1)+'日',
+            amount      : (Math.random(0, 1)*10000),
+           // error:{type:'购买失败', msg:'账户余额不足'}
           })
         }
         setTimeout(() => {
-          this.random = this.random.concat(random)
-          this.loadMore=false
-        }, 2500)
+          this.random[this.currentTab-1] = this.random[this.currentTab-1].concat(random)
+          this.loadMore = false
+        }, 1000)
       }
     },
     watch      : {
@@ -82,6 +111,41 @@
 
 </script>
 <style lang="stylus" scoped>
+  @import "../../style/base.styl"
   span.rotate
     transform rotate(180deg)
+
+  .assets-summary
+    background striking-text-color
+    font-size extra-extra-small
+    padding 10px 0
+
+  .assets-summary > div
+    color white
+    flex 1
+    text-align center
+
+  .assets-summary > div:nth-child(2), .assets-summary > div:nth-child(3)
+    border-left 1px solid
+
+  .tab-section
+    padding-top 10px
+    background white
+
+  .tab-section > div
+    flex 1
+    text-align center
+    color striking-text-color
+
+  .tab-section > div.active
+    border-bottom 2px solid
+    color primary-text-color
+
+  .operate-btn
+    font-size small
+    border 1px solid
+    color primary-text-color
+    border-radius 1em
+    padding 2px 8px
+
 </style>
